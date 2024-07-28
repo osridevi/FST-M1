4a87d681c333273c364d2b2261f619b7796cede7
osridevi <osridevi@in.ibm.com>
2024-07-28 22:43:34 +0530

    Create Activity5.java

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,14 @@
+package activities;
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		
+		String title ="This is java Book";
+		MyBook javaBook = new MyBook();
+		javaBook.setTitle(title);
+		System.out.print(javaBook.getTitle());
+
+	}
+
+}
