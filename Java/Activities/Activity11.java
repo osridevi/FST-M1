d653a685e288a468a221dbf99ae71825be7db167
osridevi <osridevi@in.ibm.com>
2024-07-28 22:49:02 +0530

    Create Activity11.java

---
+++ Java/Activities/Activity11.java
@@ -0,0 +1,27 @@
+package activities;
+
+import java.util.HashMap;
+
+public class Activity11 {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> colours = new HashMap<Integer, String>();
+		colours.put(1, "Red");
+		colours.put(2, "Green");
+		colours.put(3, "Blue");
+		colours.put(4, "White");
+		colours.put(5, "Black");
+
+		System.out.println("The Original map: " + colours);
+		colours.remove(4);
+		System.out.println("After removing White: " + colours);
+		if (colours.containsValue("Green")) {
+			System.out.println("Green exists in the Map");
+		} else {
+			System.out.println("Green does not exist in the Map");
+		}
+
+		System.out.println("Number of pairs in the Map is: " + colours.size());
+	}
+
+}
